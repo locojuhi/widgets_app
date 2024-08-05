b10675d7c0e84152d9864c8dfd575e4481f73c0e
Danny Torres <danny.torresxd@gmail.com>
2024-08-05 18:33:01 -0500

    Confirm projects works on VScode

--- lib/presentations/screens/animated/animated_screen.dart
+++ lib/presentations/screens/animated/animated_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-
 class AnimatedScreen extends StatelessWidget {
   static const String routeName = 'animated_screen';
-  const AnimatedScreen ({super.key});
+  const AnimatedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/presentations/screens/progress/progress_screen.dart
+++ lib/presentations/screens/progress/progress_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-
 class ProgressScreen extends StatelessWidget {
   static const String routeName = 'progress_screen';
-  const ProgressScreen ({super.key});
+  const ProgressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Progress Indicators'),
       ),
@@ -24,12 +23,19 @@ class _ProgressView extends StatelessWidget {
     return const Center(
       child: Column(
         children: [
-          SizedBox( height: 30,),
+          SizedBox(
+            height: 30,
+          ),
           Text('Circular progress indicator'),
-          CircularProgressIndicator(strokeWidth: 2, backgroundColor: Colors.black45,),
-          SizedBox( height: 30,),
+          CircularProgressIndicator(
+            strokeWidth: 2,
+            backgroundColor: Colors.black45,
+          ),
+          SizedBox(
+            height: 30,
+          ),
           Text('Progress indicator controlado'),
-          _ControllerProgressIndicator()
+          _ControllerProgressIndicator(),
         ],
       ),
     );
@@ -42,31 +48,32 @@ class _ControllerProgressIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: Stream.periodic(const Duration(milliseconds: 300), (value){
-        return (value * 2) / 100;
-      }).takeWhile((value) => value < 100),
-      builder: (context, snapshot) {
-        final progressValue = snapshot.data ?? 0.0;
+        stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+          return (value * 2) / 100;
+        }).takeWhile((value) => value < 100),
+        builder: (context, snapshot) {
+          final progressValue = snapshot.data ?? 0.0;
 
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircularProgressIndicator(
-                strokeWidth: 2,
-                backgroundColor: Colors.black45,
-                value: progressValue,
-              ),
-              const SizedBox(width: 20,),
-              Expanded(
-                child: LinearProgressIndicator(
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(
+                  strokeWidth: 2,
+                  backgroundColor: Colors.black45,
+                  value: progressValue,
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                    child: LinearProgressIndicator(
                   value: progressValue,
                 ))
-            ],
-          ),
-        );
-      }
-    );
+              ],
+            ),
+          );
+        });
   }
 }
