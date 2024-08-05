a124d779bcd293432b04f396a03fd0869d2baf6b
Danny Torres <danny.torresxd@gmail.com>
2024-08-04 23:29:56 -0500

    Video 131

---
+++ lib/presentations/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class SnackbarScreen extends StatelessWidget {
+  static const String routeName = 'snackbar_screen';
+  const SnackbarScreen ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
