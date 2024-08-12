b661d251f8a95eb5c717be34c3ba4aafe1c8e64c
Danny Torres <danny.torresxd@gmail.com>
2024-08-12 00:05:12 -0500

    Video 139

--- lib/presentations/screens/snackbar/snackbar_screen.dart
+++ lib/presentations/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const String routeName = 'snackbar_screen';
@@ -15,6 +16,25 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('¿Estas seguro?'),
+              content: const Text('Lorem ipsum dolore, lorem ipsum dolore'),
+              actions: [
+                TextButton(
+                  onPressed: () => context.pop(),
+                  child: const Text('Cancelar'),
+                ),
+                FilledButton(
+                    onPressed: () => context.pop(),
+                    child: const Text('Aceptar'))
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +45,25 @@ class SnackbarScreen extends StatelessWidget {
         label: const Text('Mostar Snackbar'),
         onPressed: () => showCustomSnackbar(context),
         icon: const Icon(Icons.remove_red_eye_outlined),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(
+                      context: context,
+                      children: [const Text('lorem ipsum dolore.')]);
+                },
+                child: const Text('Licencias usadas')),
+            FilledButton.tonal(
+                onPressed: () {
+                  openDialog(context);
+                },
+                child: const Text('Mostrar dialogo'))
+          ],
+        ),
       ),
     );
   }
