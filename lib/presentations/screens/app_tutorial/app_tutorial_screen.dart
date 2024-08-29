bbf96fe600539d64b8af89cdeebdce15f975fad5
Danny Torres <danny.torresxd@gmail.com>
2024-08-28 23:30:31 -0500

    Video 146

--- lib/presentations/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentations/screens/app_tutorial/app_tutorial_screen.dart
@@ -24,9 +24,37 @@ class Slideinfo {
   Slideinfo(this.title, this.caption, this.imageURL);
 }
 
-class AppTutorialScreen extends StatelessWidget {
+class AppTutorialScreen extends StatefulWidget {
   static const String routeName = 'tutorial_screen';
   const AppTutorialScreen({super.key});
+
+  @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  final PageController pageviewController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+    pageviewController.addListener(() {
+      final page = pageviewController.page ?? 0;
+
+      if (!endReached && page >= (slides.length - 1.5)) {
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageviewController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +63,7 @@ class AppTutorialScreen extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
+            controller: pageviewController,
             physics: const BouncingScrollPhysics(),
             children: slides
                 .map(
@@ -52,7 +81,17 @@ class AppTutorialScreen extends StatelessWidget {
               child: const Text('Salir'),
               onPressed: () => context.pop(),
             ),
-          )
+          ),
+          endReached
+              ? Positioned(
+                  bottom: 30,
+                  right: 30,
+                  child: FilledButton(
+                    onPressed: () => context.pop(),
+                    child: const Text('Comenzar'),
+                  ),
+                )
+              : const SizedBox()
         ],
       ),
     );
