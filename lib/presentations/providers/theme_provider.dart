750e504a7305fb6b1df9735b64b4890acad74ef2
Danny Torres <danny.torresxd@gmail.com>
2024-09-05 01:33:33 -0500

    Video 163

---
+++ lib/presentations/providers/theme_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final isDarkModeBoolean = StateProvider<bool>((ref) => false);
--- lib/presentations/screens/counter/counter_screen.dart
+++ lib/presentations/screens/counter/counter_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/presentations/providers/counter_provider.dart';
+import 'package:widgets_app/presentations/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const String routeName = 'counter_screen';
@@ -11,9 +12,20 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeBoolean);
 
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => {
+              ref.read(isDarkModeBoolean.notifier).update((state) => !state)
+            },
+            icon: Icon(isDarkMode
+                ? Icons.nightlight_outlined
+                : Icons.light_mode_outlined),
+          )
+        ],
         title: const Text('Counter Screen'),
       ),
       body: Center(
@@ -23,7 +35,10 @@ class CounterScreen extends ConsumerWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: () => {
+          //ref.read(counterProvider.notifier).state++
+          ref.read(counterProvider.notifier).update((state) => state + 1)
+        },
         child: const Icon(Icons.add),
       ),
     );
